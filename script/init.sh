0f8ab906f164b68ac3130db8e3710c3f5dac5fc2
tknzk <tkm.knzk@gmail.com>
2013-10-05 14:17:00 +0900

    update

--- script/init.sh
+++ script/init.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-#mysql -uroot -proot isucon < /home/isucon/mysql.alter.sql
 cat /home/isucon/script/mysql.alter.sql | mysql -uisucon isucon
--- script/mysql.alter.sql
+++ script/mysql.alter.sql
@@ -1,1 +1,3 @@
-ALTER TABLE `isucon`.`memos` ADD INDEX (`user`, `is_private`);
+ALTER TABLE `isucon`.`memos` MODIFY COLUMN is_private TINYINT(1);
+ALTER TABLE `isucon`.`memos` ADD INDEX user_is_private_idx (`user`, `is_private`);
+ALTER TABLE `isucon`.`memos` ADD INDEX is_private_idx (`is_private`);
