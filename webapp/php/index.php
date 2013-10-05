5079d2240b84559468c30323059d7829154cab4a
tknzk <tkm.knzk@gmail.com>
2013-10-05 14:08:38 +0900

    update

--- webapp/php/index.php
+++ webapp/php/index.php
@@ -184,7 +184,7 @@ dispatch_get('/signin', function() {
 });
 
 dispatch_post('/signout', function() {
-    session_start();
+    //session_start();
     session_regenerate_id(TRUE);
     unset($_SESSION['user_id']);
     unset($_SESSION['token']);
