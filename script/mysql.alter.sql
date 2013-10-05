a818268b0e510f622b412b76faca6ef76b424671
tknzk <tkm.knzk@gmail.com>
2013-10-05 14:26:31 +0900

    alter

--- script/mysql.alter.sql
+++ script/mysql.alter.sql
@@ -1,3 +1,4 @@
 ALTER TABLE `isucon`.`memos` MODIFY COLUMN is_private TINYINT(1);
 ALTER TABLE `isucon`.`memos` ADD INDEX user_is_private_idx (`user`, `is_private`);
 ALTER TABLE `isucon`.`memos` ADD INDEX is_private_idx (`is_private`);
+ALTER TABLE `isucon`.`users` ADD INDEX users_username_12_idx (`username`(12));
